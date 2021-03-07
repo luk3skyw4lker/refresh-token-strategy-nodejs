759568e6ecdace9f006696e1cd9d884faa7686e4
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2021-03-07 17:20:39 -0300

    [feat]: Add auth_middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import jwt from 'express-jwt';
+import { authRouter } from './routes/auth';
 
 const app = express();
+const auth_middleware = jwt({
+	secret: 'SECRET',
+	userProperty: 'payload',
+	algorithms: ['HS256']
+}).unless({ path: ['/login', '/users', '/refresh'] });
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(auth_middleware);
+
+app.use(authRouter);
 
 export { app };
